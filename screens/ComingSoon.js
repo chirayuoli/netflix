51773db59991486d203de6ef09a6b11fe8922edd
Chirayu Oli <chirayuoli56@gmail.com>
2020-10-30 20:04:10 +0545

    Created a new Expo app

---
+++ screens/ComingSoon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ComingSoon() {
+    return (
+        <View>
+            <Text>This is Coming Soon</Text>
+        </View>
+    )
+}
---
+++ screens/Downloads.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Downloads() {
+    return (
+        <View>
+            <Text>This is Downloads</Text>
+        </View>
+    )
+}
